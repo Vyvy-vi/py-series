21abfbc47c7d3a84582c707ae08dc3e8935c0797
Vy <vyom.j@protonmail.com>
2023-09-29 11:35:17 +0530

    fix: gps

--- py_series/gps.py
+++ py_series/gps.py
@@ -10,5 +10,5 @@ if __name__ == "__main__":
     a = int(input("Enter the first term: "))
     r = int(input("Enter the common ratio: "))
     n = int(input("Enter the no. of terms you want to calulate the sum to: "))
-    m, n = gps(a, r, n)
+    m, n = generate_gp(a, r, n)
     print(f"The series is given by:\n{m}...\nThe sum of the GP is:\n{n}")
